eed8b85a9ddea42a6bfe5f6d6b52e9a5b0072600
muratsat <muratsat@users.noreply.github.com>
2023-02-26 12:37:37 +0600

    renamed plugin from 'rickroll' to 'rickroll-nvim'

--- lua/rickroll-nvim.lua
+++ lua/rickroll-nvim.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.name = "rickroll"
+M.name = "rickroll-nvim"
 M.description = "RickRoll yourself in neovim"
-M.version = "0.1.0"
+M.version = "0.0.0"
 M.license = "MIT"
 M.author = "Murat <muratsatybaldiev2004@gmail.com>"
 M.dependencies = {}
