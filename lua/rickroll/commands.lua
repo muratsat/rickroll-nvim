74af34e13957033c0192f0477084e5b4d7be68d6
muratsat <muratsat@users.noreply.github.com>
2023-02-26 14:07:10 +0600

    add lyrics

--- lua/rickroll/commands.lua
+++ lua/rickroll/commands.lua
@@ -3,10 +3,10 @@ local api = vim.api
 local M = {}
 
 function M.RickRoll()
+  local lines = require('rickroll.lyrics').GetLyrics()
+
   local buffer = api.nvim_create_buf(false, true)
-  api.nvim_buf_set_lines(buffer, 0, -1, true,
-    { "never", "gonna", "give", "you", "up" }
-  )
+  api.nvim_buf_set_lines(buffer, 0, -1, true, lines)
   api.nvim_set_current_buf(buffer)
 end
 
